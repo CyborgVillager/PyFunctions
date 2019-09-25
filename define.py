3a33bbb8bf631646e3cfef2bd7737d60d04dd0a5
CyborgVillager <almawijonathan@gmail.com>
2019-09-25 11:08:56 -0500

    defining

---
+++ define.py
@@ -0,0 +1,10 @@
+'''''''''''''''
+Function name should be descriptive of the task carried by the function
+Often includes a verb
+
+def function_name():
+    statement
+    statement
+    statement
+
+'''''''''''''''
