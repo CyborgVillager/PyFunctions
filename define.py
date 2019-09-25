15b0037d814b96f1212831f246c1eb183cb36e31
CyborgVillager <almawijonathan@gmail.com>
2019-09-25 11:11:18 -0500

    Update define

--- define.py
+++ define.py
@@ -6,5 +6,17 @@ def function_name():
     statement
     statement
     statement
+    
+function header: first line of function
+e.g def function_name():
 
+Block: set of statements that blong together as a group
+e.g statement
+    statement
+    statement
+
+void function - no data returns
+
+value-returning function - executes the statment it contains
+usually its the input, int, and float functions are value returning functions.
 '''''''''''''''
