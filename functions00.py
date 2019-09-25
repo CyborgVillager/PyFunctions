b85c8a12a219eacd6a4f08f65394ff16c3222a1a
CyborgVillager <almawijonathan@gmail.com>
2019-09-25 11:07:17 -0500

    function example

--- functions00.py
+++ functions00.py
@@ -1,4 +1,10 @@
-info = int(input('test'))
-print(info)
-print(info)
-print(info)
+def divideByPositiveNumber(numerator, denominator):
+    if denominator >0:
+        return numerator/denominator
+    else:
+        return -1
+total_compensation = float(input('Please enter your total income: '))
+hours_worked = int(input('How many hours have you worked? '))
+
+hourlyPay = divideByPositiveNumber(total_compensation, hours_worked)
+print('Hourly Pay is $' + str(round(hourlyPay)))
