cbc218ffb7b52c130e3495d75ac3be740f90f8a6
CyborgVillager <almawijonathan@gmail.com>
2019-09-25 12:02:08 -0500

    global cosntant information

--- global_variable.py
+++ global_variable.py
@@ -4,6 +4,8 @@ myValue = int(input('Enter a number to be \n' +
 #Never use global variable security risk, only use it if you want a constant
 #like pi, tax rate, etc stuff that doesnt change or doesnt change for a long time
 #HOwever it adds complexity to your program, all your subsystems depends on that 'global variable'
+#more use of this will create spageti code
+#global constant on the other hand is a good thing like myValue=10
 
 def showValue():
     print(myValue)
