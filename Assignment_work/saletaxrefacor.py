f47d2487c0d7f82f0fb442447de674dc250156f1
CyborgVillager <almawijonathan@gmail.com>
2019-09-28 16:04:26 -0500

    will work on saletaxref

--- Assignment_work/saletaxrefacor.py
+++ Assignment_work/saletaxrefacor.py
@@ -1,24 +1,56 @@
 #reformat sales tax to include functions chap 5 pg 229
 
-# ask user for merchandise information
-ask_user_for_merchandise = input('Please enter the sellers merchandise name: ')
-ask_user_for_purchase = float(input('What is the dollar amount of purchase, for '
-                                    + ask_user_for_merchandise + '\n'
-                                                                 'the seller would like to list it for?: '))
 
-user_amount_of_purchase = float( input('Please enter amount of purchase: '))
+def ask_user_for_purchase():
+    # ask user for merchandise information
 
-state_tax = .05 * user_amount_of_purchase
-county_tax = .025 * user_amount_of_purchase
-total_tax = state_tax + county_tax
-combined_tax = state_tax + county_tax
-total_bill = user_amount_of_purchase + total_tax
+    ask_user_for_purchase = float(input('What is the dollar amount of purchase, for '))
 
-print('The Amount of Purchase is: $', round(user_amount_of_purchase))
-print('County and state taxes are included for ', ask_user_for_merchandise)
-print('County tax is: $', round(county_tax))
-print('State tax is: $', round(state_tax))
-print('Combined tax is: $', round(combined_tax))
-print('Total Bill is: $', round(total_bill))
-input('Thank you for using sales tax app, please press' + '\n'
-      + 'Enter, to exit')
+    #once system has the amount return it
+    return ask_user_for_purchase
+#
+def state_tax_calc( ask_user_for_purchase ):
+    #create a var and calc the user input
+    state_tax = .05 * ask_user_for_purchase
+    return state_tax
+
+def county_tax( ask_user_for_purchase ):
+    county_tax = .025 * ask_user_for_purchase
+    return county_tax
+
+                    #could be any var for the function
+def total_tax_calc(state_tax, county_tax ):
+    total_tax = state_tax + county_tax
+    return total_tax
+
+def total_bill(ask_user_for_purchase, total_tax):
+    total_bill = ask_user_for_purchase + total_tax
+    return total_bill
+
+def result_details(user_amount_of_purchase,calculate_state_tax,calculate_county_tax, \
+                   calculate_total_tax, cal_total_sale):
+    print('The Amount of Purchase is: $', format(user_amount_of_purchase, ',.2f'), \
+          'The State Taxt is: $', format(calculate_state_tax, ',.2f'), \
+          'The County Tax  is: $', format(calculate_county_tax, ',.2f'), \
+          'The total tax is: $', format(calculate_total_tax, ',.2f'), \
+          'The total sale is: $', format(cal_total_sale, ',.2f'), sep='\n')
+
+
+
+
+
+
+def main():
+    user_amount_of_purchase = ask_user_for_purchase()
+    calculate_state_tax = state_tax_calc(str(ask_user_for_purchase))
+    calculate_county_tax = county_tax(str(ask_user_for_purchase ))
+    calculate_total_tax = total_tax_calc(calculate_state_tax, calculate_county_tax)
+    cal_total_sale = total_bill(user_amount_of_purchase,calculate_total_tax)
+    result_details(user_amount_of_purchase,calculate_state_tax,calculate_county_tax, \
+                   calculate_total_tax, cal_total_sale)
+
+
+
+
+
+main()
